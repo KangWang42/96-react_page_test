a3d931a2cf74efa42aa259ad6811cdff3aba4fe7
WangKang <888666wang4286@gmail.com>
2024-10-14 22:03:57 +0800

    首次

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <p>ttttttttttttttttt</p>
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -18,7 +19,7 @@ export default function Home() {
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
-            .
+            
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
